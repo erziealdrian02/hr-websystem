48dc9ec689ae26204eb2809e0bebe75565a6e78d
erziealdrian02 <erzie.aldrian02@gmail.com>
2026-04-05 21:46:27 +0700

    feat: implement dynamic user profile display and secure routes behind authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -21,49 +21,47 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
-
-Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
-
-Route::get('/employees', [EmployeeController::class, 'employees'])->name('employees.index');
-Route::get('/employees/detail', [EmployeeController::class, 'employeeDetail'])->name('employees.detail');
-Route::get('/employees/form', [EmployeeController::class, 'employeeForm'])->name('employees.form');
-
-Route::get('/placement', [PlacementController::class, 'placement'])->name('placement.index');
-Route::get('/placement/form', [PlacementController::class, 'placementForm'])->name('placement.form');
-
-Route::get('/division', [DivisionController::class, 'division'])->name('division.index');
-Route::get('/division/form', [DivisionController::class, 'divisionForm'])->name('division.form');
-
-Route::get('/contract', [ContractController::class, 'contract'])->name('contract.index');
-
-Route::get('/attendance', [AttendanceController::class, 'attendance'])->name('attendance.index');
-
-Route::get('/leave', [LeaveController::class, 'leave'])->name('leave.index');
-
-Route::get('/overtime', [OvertimeController::class, 'overtime'])->name('overtime.index');
-
-Route::get('/payroll', [PayrollController::class, 'payroll'])->name('payroll.index');
-
-Route::get('/reimburse', [ReimburseController::class, 'reimburse'])->name('reimburse.index');
-
-Route::get('/performance', [PerformanceController::class, 'performance'])->name('performance.index');
-
-Route::get('/users', [UserController::class, 'users'])->name('users.index');
-
-Route::get('/profile', [ProfileController::class, 'profile'])->name('profile.index');
-
-// Admin Route
-Route::get('/leave-request', [AdminLeaveController::class, 'leaveAdmin'])->name('leave.index.admin');
-Route::get('/all-attendance', [AdminAttendanceController::class, 'attendanceAdmin'])->name('attendance.index.admin');
-Route::get('/overtime-request', [AdminOvertimeController::class, 'overtimeAdmin'])->name('overtime.index.admin');
-Route::get('/all-payroll', [AdminPayrollController::class, 'payrollAdmin'])->name('payroll.index.admin');
-Route::get('/reimburse-approval', [AdminReimburseController::class, 'reimburseAdmin'])->name('reimburse.index.admin');
 
 Route::middleware('auth')->group(function () {
     // Rute Employee Management
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
 
+    Route::get('/employees', [EmployeeController::class, 'employees'])->name('employees.index');
+    Route::get('/employees/detail', [EmployeeController::class, 'employeeDetail'])->name('employees.detail');
+    Route::get('/employees/form', [EmployeeController::class, 'employeeForm'])->name('employees.form');
+
+    Route::get('/placement', [PlacementController::class, 'placement'])->name('placement.index');
+    Route::get('/placement/form', [PlacementController::class, 'placementForm'])->name('placement.form');
+
+    Route::get('/division', [DivisionController::class, 'division'])->name('division.index');
+    Route::get('/division/form', [DivisionController::class, 'divisionForm'])->name('division.form');
+
+    Route::get('/contract', [ContractController::class, 'contract'])->name('contract.index');
+
+    Route::get('/attendance', [AttendanceController::class, 'attendance'])->name('attendance.index');
+
+    Route::get('/leave', [LeaveController::class, 'leave'])->name('leave.index');
+
+    Route::get('/overtime', [OvertimeController::class, 'overtime'])->name('overtime.index');
+
+    Route::get('/payroll', [PayrollController::class, 'payroll'])->name('payroll.index');
+
+    Route::get('/reimburse', [ReimburseController::class, 'reimburse'])->name('reimburse.index');
+
+    Route::get('/performance', [PerformanceController::class, 'performance'])->name('performance.index');
+
+    Route::get('/users', [UserController::class, 'users'])->name('users.index');
+
+    Route::get('/profile', [ProfileController::class, 'profile'])->name('profile.index');
+
+    // Admin Route
+    Route::get('/leave-request', [AdminLeaveController::class, 'leaveAdmin'])->name('leave.index.admin');
+    Route::get('/all-attendance', [AdminAttendanceController::class, 'attendanceAdmin'])->name('attendance.index.admin');
+    Route::get('/overtime-request', [AdminOvertimeController::class, 'overtimeAdmin'])->name('overtime.index.admin');
+    Route::get('/all-payroll', [AdminPayrollController::class, 'payrollAdmin'])->name('payroll.index.admin');
+    Route::get('/reimburse-approval', [AdminReimburseController::class, 'reimburseAdmin'])->name('reimburse.index.admin');
 
     // Rute Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
